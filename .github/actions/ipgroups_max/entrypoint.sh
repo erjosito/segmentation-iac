236bf3a4fb39d95289680b3125c860750719b32b
Jose Moreno <jose.moreno@microsoft.com>
2023-05-30 21:50:51 +0200

    moved action

---
+++ .github/actions/ipgroups_max/entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/sh -l
+
+# Initialization
+file_prefix="$1"
+file_extension="$2"
+max_ipgroup_no="$3"
+ipgroup_no=0
+
+# Info message
+echo "DEBUG: Running with file_prefix=$file_prefix, file_extension=$file_extension."
+
+# Find all files with the given prefix and extension
+file_list=$(find . -type f -name "$file_prefix*$file_extension" -print)
+
+# Go file by file and count the number of IP groups
+for file in $file_list; do
+    this_ipgroup_no=$(cat $file | jq -r '.resources[].name' | wc -l)
+    echo "DEBUG: Found $this_ipgroup_no IP groups in $file."
+    ipgroup_no=$((ipgroup_no + this_ipgroup_no))
+done
+
+# Finish
+if [ "$ipgroup_no" -gt "$max_ipgroup_no" ]; then
+    echo "ERROR: Found $ipgroup_no IP groups, which is more than the maximum of $max_ipgroup_no."
+    exit 1
+else
+    echo "INFO: Found $ipgroup_no IP groups, which is not more than the maximum of $max_ipgroup_no."
+    exit 0
+fi
