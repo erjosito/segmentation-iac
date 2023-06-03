615222012dd86861fe8ffd6ac79546280d64eb30
Jose Moreno <jose.moreno@microsoft.com>
2023-06-03 08:52:19 +0200

    bugfix

--- .github/actions/fw_rule_prio_bicep/entrypoint.py
+++ .github/actions/fw_rule_prio_bicep/entrypoint.py
@@ -7,6 +7,7 @@ from pycep import BicepParser
 # Global variables
 min_prio_found = None
 max_prio_found = None
+prios_found = []
 
 # Read parameters
 try:
@@ -30,7 +31,7 @@ try:
 except:
     base_dir = "./"
 
-print("DEBUG: Running with file_prefixes='{0}', file_extension='{1}', min_cidr_length={2}, base_dir='{3}'".format(file_prefixes, file_extension, min_cidr_length, base_dir))
+print("DEBUG: Running with file_prefixes='{0}', file_extension='{1}', min_prio={2}, max_prio={3}, base_dir='{4}'".format(file_prefixes, file_extension, min_prio, max_prio, base_dir))
 
 # Read files
 file_prefix_list=file_prefixes.split(' ')
@@ -48,14 +49,20 @@ for file_prefix in file_prefix_list:
                     if resource['type'].lower() == 'Microsoft.Network/firewallPolicies/ruleCollectionGroups'.lower():
                         if 'config' in resource and 'properties' in resource['config'] and 'priority' in resource['config']['properties']:
                             rcg_prio = int(resource['config']['properties']['priority'])
+                            # Update min/max prio found
                             if min_prio_found is None or rcg_prio < min_prio_found:
                                 min_prio_found = rcg_prio
                             if max_prio_found is None or rcg_prio > max_prio_found:
                                 max_prio_found = rcg_prio
+                            # Show info messages for this RCG
                             if rcg_prio < min_prio:
                                 print("ERROR: Found rule collection group '{0}' with priority {1}, which is lower than the minimum defined of {2}".format(resource_key, rcg_prio, min_prio))
                             if rcg_prio > max_prio:
                                 print("ERROR: Found rule collection group '{0}' with priority {1}, which is higher than the maximum defined of {2}".format(resource_key, rcg_prio, max_prio))
+                            # Update prio found array and check for duplicates
+                            if rcg_prio in prios_found:
+                                print("ERROR: Found rule collection group '{0}' with priority {1}, which is already used".format(resource_key, rcg_prio))
+                            prios_found.append(rcg_prio)
                         else:
                             print("WARNING: No priority found in resource '{0}' in file '{1}'".format(resource_key, file))
                         rcs = resource['config']['properties']['ruleCollections']
@@ -65,14 +72,20 @@ for file_prefix in file_prefix_list:
                                 print("DEBUG: Found rule collection '{0}' in file '{1}'".format(rc['name'], file))
                                 if 'priority' in rc:
                                     rc_prio = int(rc['priority'])
+                                    # Update min/max prio found
                                     if min_prio_found is None or rc_prio < min_prio_found:
                                         min_prio_found = rc_prio
                                     if max_prio_found is None or rc_prio > max_prio_found:
                                         max_prio_found = rc_prio
+                                    # Show info messages for this RC
                                     if rc_prio < min_prio:
                                         print("ERROR: Found rule collection '{0}' with priority {1}, which is lower than the minimum defined of {2}".format(rc['name'], rc_prio, min_prio))
                                     if rc_prio > max_prio:
                                         print("ERROR: Found rule collection '{0}' with priority {1}, which is higher than the maximum defined of {2}".format(rc['name'], rc_prio, max_prio))
+                                    # Update prio found array and check for duplicates
+                                    if rc_prio in prios_found:
+                                        print("ERROR: Found rule collection group '{0}' with priority {1}, which is already used".format(resource_key, rcg_prio))
+                                    prios_found.append(rc_prio)
                                 else:
                                     print("WARNING: No priority found in rule collection '{0}' in file '{1}'".format(rc['name'], file))
                             else:
@@ -88,7 +101,11 @@ if min_prio_found < min_prio:
 elif max_prio_found > max_prio:
     print("ERROR: Highest prio found was {0}, which is larger than {1}".format(max_prio_found, max_prio))
     exit(1)
+elif len(prios_found) != len(set(prios_found)):
+    print("ERROR: Duplicate priorities found across rule collections and rule collection groups")
+    exit(1)
 else:
     print("INFO: Priorities found between {0} and {1}, in the allowed range ({2}-{3})".format(min_prio_found, max_prio_found, min_prio, max_prio))
+    print("INFO: No duplicate priorities found across rule collections and rule collection groups")
     exit(0)
 
